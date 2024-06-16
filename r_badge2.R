5eb2ac5d5397c05f875273cf75df2d306a533e2f
Richard 'Ricky' Kuehn <richardprinzkuehn@gmail.com>
2024-06-15 22:07:43 -0400

    python finished

--- r_badge2.R
+++ r_badge2.R
@@ -1,2 +1,15 @@
 install.packages("tidyverse")
 library(tidyverse)
+
+## test ggplot
+ggplot(mtcars, aes(x = wt, y = mpg)) + geom_point()
+
+# Euler 1
+answer <- 0
+for (i in 1:999) {
+  if (i %% 3 == 0 | i %% 5 == 0) {
+    answer <- answer + i
+    print(i)
+  }
+}
+print(answer)
